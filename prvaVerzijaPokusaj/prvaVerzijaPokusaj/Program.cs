f95ea10d94ca5c7ea0b797bc47e15b87912c47ce
plavi01 <plavi01@gmail.com>
2018-08-23 19:16:45 +0200

    initial commit

---
+++ prvaVerzijaPokusaj/prvaVerzijaPokusaj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prvaVerzijaPokusaj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
