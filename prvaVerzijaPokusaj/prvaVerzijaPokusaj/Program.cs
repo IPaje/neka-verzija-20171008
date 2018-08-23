c30baf9e9782f018cd8fb2730adabb7358cb531b
plavi01 <plavi01@gmail.com>
2018-08-23 19:22:38 +0200

    drugi komit

--- prvaVerzijaPokusaj/prvaVerzijaPokusaj/Program.cs
+++ prvaVerzijaPokusaj/prvaVerzijaPokusaj/Program.cs
@@ -6,7 +6,7 @@ namespace prvaVerzijaPokusaj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2222!");
         }
     }
 }
